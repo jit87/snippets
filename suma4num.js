3411d4da9ea6701ba289ad6ad762bafe4b03d59a
Ignacio <ignaciotejada87@gmail.com>
2024-03-27 01:17:42 +0100

    Upload 27/03/2024 1

---
+++ suma4num.js
@@ -0,0 +1,32 @@
+//Variable temporal para almacenar el valor antes de que se modifique
+var temp; 
+
+//Bandera para saber si hay que continuar con el bucle
+var hayIntercambio;
+
+//Variables de sumas
+var sumaMin = 0;
+var sumaMax = 0; 
+
+
+do {
+    hayIntercambio = false;
+    //Ordenamos de menor a mayor
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i+1] < arr[i]){
+             temp = arr[i];
+             arr[i] = arr[i+1]; 
+             arr[i+1] = temp;
+             hayIntercambio = true;  
+        }
+    }
+} while (hayIntercambio);
+
+
+//Como tenemos sumar 4 numeros menores y 4 mayores lo hacemos directamente:
+sumaMin = arr[0] + arr[1] + arr[2] + arr[3];
+sumaMax = arr[1] + arr[2] + arr[3] + arr[4];
+
+console.log("Vector ordenado: " + arr);
+console.log("Suma de los menores: " + sumaMin);
+console.log("Suma de los mayores: " + sumaMax);

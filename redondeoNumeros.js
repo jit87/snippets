735c81cea845568eac042688c54032699ea6b127
Ignacio <ignaciotejada87@gmail.com>
2024-04-23 22:36:47 +0200

    Upload 23/04/2024 5

--- redondeoNumeros.js
+++ redondeoNumeros.js
@@ -19,14 +19,14 @@ function redondeoNumeros(valores) {
           if(ultimoDigito <= 5){
               diferencia = 5 - ultimoDigito;       
           } 
-           //Si el último dígito es mayor a 5 se redondeará a 0 (10) la última cifra si cumple las condiciones requeridas
+          //Si el último dígito es mayor a 5 se redondeará a 0 (10) la última cifra si cumple las condiciones requeridas
           if(ultimoDigito > 5){
               diferencia = 10 - ultimoDigito;
           }
-         //Si la diferencia es menor o igual a 2 aplicamos el redondeo
-         if(diferencia <= 2) {
+          //Si la diferencia es menor o igual a 2 aplicamos el redondeo
+          if(diferencia <= 2) {
              arrResult.push(valores[i] + diferencia);
-         } else 
+          } else 
              arrResult.push(valores[i]);   
     }
       console.log(arrResult);

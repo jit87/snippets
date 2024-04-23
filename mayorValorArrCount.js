cb762153b6f4c86cad80bc90acddd5c314338bd6
Ignacio <ignaciotejada87@gmail.com>
2024-04-23 14:26:49 +0200

    Upload 23/04/2024 3

--- mayorValorArrCount.js
+++ mayorValorArrCount.js
@@ -1,6 +1,6 @@
 //Algoritmo que devuelve el numero de veces del valor mayor del vector
 
-var velas = [3000,5,7,9,2000,500,3000];
+var vector = [3000,5,7,9,2000,500,3000];
 var temp = 0; 
 var terminado;
 var count = 0; 
@@ -8,11 +8,11 @@ var count = 0;
 //Ordenamos de menor a mayor los números del vector
 do{
    terminado = false;
-   for(var i = 0; i < velas.length; i++){
-       if(velas[i+1] < velas[i]){
-          temp = velas[i];
-          velas[i] = velas[i+1]; 
-          velas[i+1] = temp;
+   for(var i = 0; i < vector.length; i++){
+       if(vector[i+1] < vector[i]){
+          temp = vector[i];
+          vector[i] = vector[i+1]; 
+          vector[i+1] = temp;
           terminado = true; 
       }
    }
@@ -20,8 +20,8 @@ do{
 } while(terminado);
 
 //Contamos la cantidad de valores mayores
-for(var i = 0; i < velas.length; i++){
-    if(velas[i]===velas[velas.length-1]){
+for(var i = 0; i < vector.length; i++){
+    if(vector[i]===vector[vector.length-1]){
         count++;
     }
 }
